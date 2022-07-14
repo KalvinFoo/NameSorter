1c4278559ddc921819617974af9d2f8cb8e3146b
Kalvin Foo <kalvinf@weefer.co.id>
2022-07-14 18:34:36 +0700

    Initial commit

---
+++ NameSorterTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NameSorterTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NameSorterTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("073351b0-c726-4474-a834-5d1155f2565f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter
+{
+    public class NameSorters
+    {
+        static readonly string inputFileName = "unsorted-names-list.txt";
+        static readonly string outputFileName = "sorted-names-list.txt";
+        static readonly string fileDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        public static void Main(string[] args)
+        {
+            var nameSorter = new NameSorters();
+            var result = nameSorter.ReadAndSortByLastName(inputFileName);
+
+            Array.ForEach(result, Console.WriteLine);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Return names sorted by last name, then by full name
+        /// </summary>
+        /// <param name="prmInputFileName">The source text file containing the names that need to be sorted</param>
+        /// <returns>Sorted names as string array</returns>
+        public string[] ReadAndSortByLastName(string prmInputFileName)
+        {
+            string inputFilePath = Path.Combine(fileDirectory, prmInputFileName);
+            string outputFilePath = Path.Combine(fileDirectory, outputFileName);
+
+            if (!File.Exists(inputFilePath))
+            {
+                throw new Exception("'File is not found. Please check again...");
+            }
+
+            string[] names = File.ReadLines(inputFilePath).ToArray(); //Read source text file
+
+            var sortingResult = names.OrderBy(x => x.Split(' ').Last()) //Sort by last name, then by full name
+                .ThenBy(n => n)
+                .ToArray();
+
+            File.WriteAllLines(outputFilePath, sortingResult); //Write or overwrite the result into the targeted text file
+
+            return sortingResult;
+        }
+    }
+}
